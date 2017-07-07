eecd82f3d3d3cf53043a8b5ae348af000ee35516
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-07 18:22:03 +0530

    Read the HTML table and converted to a R Dataframe

--- WebScrape.R
+++ WebScrape.R
@@ -2,9 +2,26 @@
 #Loading the packages
 
 require(rvest)
+#rvest is the package to scrape Web pages in R
 require(ggplot2)
 require(dplyr)
 require(scales)
 require(maps)
 require(mapproj)
+
+
+
+#Loading the Data--------------
+
+obesity<-read_html("https://en.m.wikipedia.org/wiki/Obesity_in_India")
+
+#html_nodes() to select a particular HTML element from the above page
+#table because we want to select a "table"
+
+#Converting to a R dataframe
+obesity = obesity %>%
+  html_nodes("table") %>% 
+  .[[1]] %>%
+  html_table()
+
 
