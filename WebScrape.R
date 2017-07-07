f1709b4690a1725d5531c55614b325d11b02033d
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-07 16:58:47 +0530

    Required the packages needed

--- WebScrape.R
+++ WebScrape.R
@@ -1,1 +1,10 @@
 #Visualizing obesity across India and Web scraping using R
+#Loading the packages
+
+require(rvest)
+require(ggplot2)
+require(dplyr)
+require(scales)
+require(maps)
+require(mapproj)
+
